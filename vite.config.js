dd7add4bc4b0bf56f3f82ea4ca972e846ec175e6
Daniil Postnov <daniilpostnov@MacBook-Pro-Daniil.local>
2025-01-13 18:29:01 +0500

    fix: add sound files to build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: './', // Используем относительные пути
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    // Копируем звуки в dist
+    assetsInclude: ['**/*.wav'],
+  },
 }) 
